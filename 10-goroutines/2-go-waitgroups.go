49935094074be955fb4b38f14dab4231ef58b0e7
Diwakar Singh <diwakarsingh052@gmail.com>
2025-07-15 10:35:57 +0530

    .

---
+++ 10-goroutines/2-go-waitgroups.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	//func(a, b int) int { // a func without name
+	//anonymous function
+	//	return a + b
+	//}(10, 20) // () is calling the func
+	wg := new(sync.WaitGroup)
+	//wg := &sync.WaitGroup{}
+
+	// waitgroup counter represents number of goroutine we are running
+	wg.Add(1) // add 1 to the counter
+	go func() {
+		time.Sleep(time.Second * 2)
+		fmt.Println("hello world")
+		wg.Done()
+	}()
+	fmt.Println("doing more things in main")
+	wg.Wait() // it will block until counter is not 0
+
+	fmt.Println("end of main")
+}
