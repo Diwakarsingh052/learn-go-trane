78d0407d9dc4a73e4436f0f1064832cf7e1497b6
Diwakar Singh <diwakarsingh052@gmail.com>
2025-07-15 11:14:28 +0530

    .

--- 10-goroutines/3-go.go
+++ 10-goroutines/3-go.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	wg := new(sync.WaitGroup)
 
-	for i := 1; i <= 10; i++ {
+	for i := 1; i <= 5; i++ {
 		wg.Add(1)
 		go work(i, wg)
 	}
@@ -20,7 +20,9 @@ func work(id int, wg *sync.WaitGroup) {
 	defer wg.Done()
 
 	fmt.Println("work is going on", id)
+	wg.Add(1)
 	go func() {
+		defer wg.Done()
 		fmt.Println("sub work is going on", id)
 	}()
 }
