f8838d5de1ea866a56007a2bebb53c744a5187b6
Diwakar Singh <diwakarsingh052@gmail.com>
2025-07-15 10:28:22 +0530

    .

--- 10-goroutines/1-go.go
+++ 10-goroutines/1-go.go
@@ -1,13 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	// runtime.GOMAXPROCS(1) // set number of processors to use
-	go hello()
+	go hello() // this call would be concurrent,
+	// which means hello is a new goroutine
+	time.Sleep(time.Second * 2)
 }
 
 func hello() {
-	//time.Sleep(time.Second * 2)
+	//time.Sleep(time.Second * 3)
 	fmt.Println("Hello World")
 }
