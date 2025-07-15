884ef5133b47a23553a4c9ced9d15ab718264266
Diwakar Singh <diwakarsingh052@gmail.com>
2025-07-15 12:51:00 +0530

    .

--- 10-without-goroutines/8-ranges.go
+++ 10-without-goroutines/8-ranges.go
@@ -3,18 +3,21 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
 func main() {
 	wg := &sync.WaitGroup{}
 
-	ch := make(chan int, 5)
+	ch := make(chan int)
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
 		for i := 0; i < 5; i++ {
 			ch <- i
+
 		}
+		fmt.Println("send finished")
 		close(ch) // sends a signal to stop the range
 		// close: signal range that no more values be sent and it can stop after receiving remaining values
 
@@ -23,7 +26,24 @@ func main() {
 	}()
 
 	// it would run infinitely, channel needs to be closed to stop this range
-	for v := range ch {
-		fmt.Println(v)
-	}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for v := range ch {
+			time.Sleep(time.Second * 1)
+			fmt.Println("range 1", v)
+		}
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for v := range ch {
+			time.Sleep(time.Second * 1)
+			fmt.Println("range 2", v)
+		}
+	}()
+
+	wg.Wait()
+
 }
