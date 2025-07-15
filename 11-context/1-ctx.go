880ab673ac8a12bde78dddd469d7b9d66218f45d
Diwakar Singh <diwakarsingh052@gmail.com>
2025-07-15 16:53:23 +0530

    .

--- 11-context/1-ctx.go
+++ 11-context/1-ctx.go
@@ -8,6 +8,8 @@ import (
 )
 
 func main() {
+	// context -> cancellation signals
+	//     		-> passing values in different layers of a http request
 
 	// empty container
 	// we need a container to hold the context object
@@ -15,6 +17,8 @@ func main() {
 	ctx := context.Background()
 	ctx, cancel := context.WithTimeout(ctx, time.Second)
 	defer cancel()
+	// clean up the resources taken up by the context
+	// if we don't put cancel in defer timer would be immediately cancelled
 	fmt.Println(SlowFunc(ctx, "10"))
 
 }
@@ -27,9 +31,11 @@ func SlowFunc(ctx context.Context, input string) (int, error) {
 	}
 	select {
 	case <-ctx.Done():
+		// done would receive signal in case the timer is over
 		fmt.Println("context done")
 		return 0, ctx.Err()
 	default:
+		// select would unblock immediately if done has not received the value
 		fmt.Println("time is still there")
 	}
 	return i, nil
