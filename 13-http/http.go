ea22d6877c52774e3429b74558b439082c7c04cc
Diwakar Singh <diwakarsingh052@gmail.com>
2025-07-16 11:09:48 +0530

    .

--- 13-http/http.go
+++ 13-http/http.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -35,4 +36,28 @@ func Home(w http.ResponseWriter, r *http.Request) {
 
 	}
 	w.Write([]byte("Hello World"))
+}
+
+func sendJsonResponse(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var user struct {
+		FirstName string `json:"first_name"` // field tag // we are giving what the name of field should be in json output
+		Password  string `json:"-"`          // ignoring the field in JSON output
+		Email     string `json:"email"`
+	}
+	user.FirstName = "abc"
+	user.Password = "123"
+	user.Email = "abc@gmail.com"
+
+	// json.Marshal converts a type to json
+	jsonData, err := json.Marshal(user)
+	if err != nil {
+		// sending text based error resp
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	// setting header and then sending a response
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonData)
+
 }
