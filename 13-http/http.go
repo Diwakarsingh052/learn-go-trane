3096a67f6663de75fa1c103ab12408d24723e88e
Diwakar Singh <diwakarsingh052@gmail.com>
2025-07-16 11:28:58 +0530

    .

--- 13-http/http.go
+++ 13-http/http.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 )
 
@@ -59,5 +60,38 @@ func sendJsonResponse(w http.ResponseWriter, r *http.Request) {
 	// setting header and then sending a response
 	w.WriteHeader(http.StatusOK)
 	w.Write(jsonData)
+
+}
+
+type student struct {
+	FirstName string `json:"first_name"` // json is a field tag, we are setting custom name for json output
+	Password  string `json:"-"`          // - means ignore the field in json output
+	Email     string `json:"email"`
+}
+
+func recvJson(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	if r.Method != http.MethodPost {
+		http.Error(w, "only post method is allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	//r.URL.Query().Get("name") // reading the query param
+	// r.Header.Get("Content-Type") // request header
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	var s student
+	//fmt.Println(string(body))
+	// unmarshal converts json into struct or map
+	err = json.Unmarshal(body, &s)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fmt.Printf("%+v\n", s)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("read the body"))
 
 }
